dd24deba8bee3f9e045497a84d88254a23e3c1ce
peter muthwi <peterkilungya1@gmail.com>
2023-11-28 09:36:20 +0300

    components set up done

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import About from './assets/Components/About'
+import Contact from './assets/Components/Contact'
+import Navbar from './assets/Components/Navbar'
+import Projects from './assets/Components/Projects'
+import Skills from './assets/Components/Skills'
+import Testimonials from './assets/Components/Testimonials'
 
 function App() {
   return (
-    <div>hello peter</div>
-  )
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
 }
-
-export default App
+export default App;
