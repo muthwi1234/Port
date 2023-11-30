1276d9356b676b22aef46bae98cf856b9974a357
peter muthwi <peterkilungya1@gmail.com>
2023-11-30 11:05:07 +0300

    updated contact

--- Portfolio/src/assets/Components/Contact.jsx
+++ Portfolio/src/assets/Components/Contact.jsx
@@ -31,12 +31,12 @@ import React from "react";
                 EMAIL
               </h2>
               <a className="text-indigo-400 leading-relaxed">
-                reedbarger@email.com
+                pinchezkeing@gmail.com
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
-              <p className="leading-relaxed">123-456-7890</p>
+              <p className="leading-relaxed">0790034942</p>
             </div>
           </div>
         </div>
