2d75998fdeb8c0c70b1fa8579e365f9636ba806f
peter muthwi <peterkilungya1@gmail.com>
2023-12-01 18:55:16 +0300

    upadated address location

--- Portfolio/src/assets/Components/Contact.jsx
+++ Portfolio/src/assets/Components/Contact.jsx
@@ -22,8 +22,8 @@ import React from "react";
                 ADDRESS
               </h2>
               <p className="mt-1">
-                97 Warren St. <br />
-                New York, NY 10007
+                Jamhuri Phase 1 <br />
+                Nairobi ,00100
               </p>
             </div>
             <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
